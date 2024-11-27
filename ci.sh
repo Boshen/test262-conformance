f1efe7bf28ec421e0e7dfb68c12ac8f75cd2abd9
Boshen <boshenc@gmail.com>
2024-11-27 18:16:32 +0800

    exclude

--- ci.sh
+++ ci.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo 'node:' `node --version`
+echo $1
 
 ./node_modules/.bin/test262-harness \
   --host-type=node \
-  --host-path=`which node` \
+  --host-path="$1" \
 	--timeout=10000 \
-	--threads=8 \
+	--threads="$(node -pe 'os.cpus().length - 1')" \
   --preprocessor ./preprocessor.js \
+  --features-exclude="module" \
+  --test262-dir=test262 \
   "test262/test/language/**/*.js" | grep -v ^PASS
